77a8a2662c72ba8be363e03c303959e237853fee
ritikramuka <ritik.ramuka@gmail.com>
2020-09-25 19:31:43 +0530

    worked on graph

--- src/LineChart.js
+++ src/LineChart.js
@@ -3,18 +3,43 @@ import Line from 'react-chartjs-2'
 
 function LineChart() {
     const [data, setData] = useState([]);
+
+    const BuildChartData = (data, casesType = "cases") => {
+        const chartData = [];
+        let lastDataPoint;
+        for(let date in data[casesType]) {
+          if(lastDataPoint) {
+            const newDataPoint = {
+              x: date,
+              y: data[casesType][date] - lastDataPoint
+            }
+            chartData.push(newDataPoint);
+          } 
+          lastDataPoint = data['cases'][date];
+        }
+        return chartData;
+      }
+
     useEffect(() => {
         fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
         .then(response => response.json())
         .then((data) => {
-            console.log(data);
+            const chartData = BuildChartData(data);
+            setData(chartData);
         })
     }, [])
 
     return (
         <div className = "lineChart">
             <h1>Im graph</h1>
-            {/* <Line></Line> */}
+            <Line data={{
+                datasets : [
+                {
+                    backgroundColor : "#ff3421",
+                    borderColor : "#f3f3ff",
+                    data: data
+                }]
+            }}></Line>
         </div>
 
 )
